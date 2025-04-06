3f41eafe4e45202e60f83801ad06e2fa37b8c9cd
Bhagya Deshmukh <deshmukhbhagyashri090@gmail.com>
2025-04-06 12:10:14 +0530

    updated

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Collect static files
+cd ctf
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+# Start Daphne
+daphne -p 8089 -b 0.0.0.0 ctf.asgi:application &
+
+# Start Nginx (assuming it's configured to serve static files)
+nginx -g "daemon off;"
